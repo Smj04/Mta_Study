decd933b5deee1eff9a185c20a7545552cbf88a0
서민정 <79045880+Smj04@users.noreply.github.com>
2022-01-03 09:56:12 +0900

    Delete Exam01.java

--- Exam01.java
+++
@@ -1,9 +0,0 @@
-
-public class Exam01 {
-	public static void main(String[] args) {
-		 double pi = Math.PI;
-		 System.out.format("Pi is %.3f%n", pi);		
-		 System.out.format("Pi is %.0f%n", pi);		
-		 System.out.format("Pi is %09f%n", pi);
-	}
-}
