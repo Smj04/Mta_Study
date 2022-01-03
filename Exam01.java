7548db9a4d03b6de7d38d9d6c36f605e643e5f92
서민정 <79045880+Smj04@users.noreply.github.com>
2022-01-03 09:56:01 +0900

    Add files via upload

---
+++ Exam01.java
@@ -0,0 +1,9 @@
+
+public class Exam01 {
+	public static void main(String[] args) {
+		 double pi = Math.PI;
+		 System.out.format("Pi is %.3f%n", pi);		
+		 System.out.format("Pi is %.0f%n", pi);		
+		 System.out.format("Pi is %09f%n", pi);
+	}
+}
